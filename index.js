dcac6560947a88df9e6163570cda99228ca0148b
Chase Van Halen <cvanhalen@perscholas.org>
2023-08-25 14:00:35 -0700

    finshed buttons

--- index.js
+++ index.js
@@ -18,3 +18,25 @@ fineButton.addEventListener('click', () => {
     // 4. add active class to fine button
     fineButton.classList.add("active-button")
 })
+happyButton.addEventListener('click', () => {
+    // 1. change img src to ./fine.jpg
+    feelingImage.src = "./assets/happy.jpg"
+    feelingImage.alt = "happy"
+    // 2. change img alt to "fine"
+    // 3. remove acive class from happy button OR sad button
+    fineButton.classList.remove('active-button')
+    sadButton.classList.remove('active-button')
+    // 4. add active class to fine button
+    happyButton.classList.add("active-button")
+})
+sadButton.addEventListener('click', () => {
+    // 1. change img src to ./fine.jpg
+    feelingImage.src = "./assets/sad.jpg"
+    feelingImage.alt = "sad"
+    // 2. change img alt to "fine"
+    // 3. remove acive class from happy button OR sad button
+    fineButton.classList.remove('active-button')
+    happyButton.classList.remove('active-button')
+    // 4. add active class to fine button
+    sadButton.classList.add("active-button")
+})
