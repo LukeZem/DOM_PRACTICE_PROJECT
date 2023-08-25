dab91da205542491ac752006db54455c6245a69f
Chase Van Halen <cvanhalen@perscholas.org>
2023-08-25 14:27:34 -0700

    finish cheerleader

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 console.log("js file is connected and running");
 
 
-let happyButton = document.querySelector(".happy");
-let fineButton = document.querySelector(".fine");
-let sadButton = document.querySelector(".not-good");
-let feelingImage = document.querySelector(".feeling-container img");
+const happyButton = document.querySelector(".happy");
+const fineButton = document.querySelector(".fine");
+const sadButton = document.querySelector(".not-good");
+const feelingImage = document.querySelector(".feeling-container img");
 const luckyNumberElement = document.getElementById('lucky-num');
-
-let randomNum = Math.floor(Math.random()*500);
+const cheerleaderParent = document.querySelector('.cheerleader')
+randomNum = Math.floor(Math.random()*500);
 
 luckyNumberElement.textContent = `Your lucky number today is ${randomNum}`
 
-console.log(luckyNumberElement);
+
+
+let cheerleaders = [
+    {
+        name: "Robert",
+        img: "https://img.freepik.com/free-vector/cute-australian-shepherd_138676-2071.jpg"
+    },
+    {
+        name: "Rex Goliath",
+        img: "https://img.freepik.com/vector-premium/t-rex-vector-logo_43623-377.jpg"
+    },
+    {
+        name: "Sir William",
+        img: "https://img.freepik.com/free-vector/white-goat-cartoon-character_1308-108587.jpg?w=2000"
+    },
+    {
+        name: "Shanaynay",
+        img: "https://img.freepik.com/premium-vector/cute-cat-vector-illustration_175152-337.jpg?w=2000"
+    },
+]
+
+
+
+let randomIndex = Math.floor(Math.random()*cheerleaders.length)
+
+// select random cheerleader
+let randomCheerleader = cheerleaders[randomIndex];
+// put on screen
+console.log(randomCheerleader);
+
+cheerleaderParent.children[0].textContent = randomCheerleader.name;
+cheerleaderParent.children[1].src = randomCheerleader.img;
+
+
+
 
 fineButton.addEventListener('click', () => {
     // 1. change img src to ./fine.jpg
