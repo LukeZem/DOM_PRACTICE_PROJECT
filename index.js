be0cd3c92152526d07d604e4ad073c672f8ed3e0
Chase Van Halen <cvanhalen@perscholas.org>
2023-08-25 15:53:04 -0700

    finish app

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const sadButton = document.querySelector(".not-good");
 const feelingImage = document.querySelector(".feeling-container img");
 const luckyNumberElement = document.getElementById('lucky-num');
 const cheerleaderParent = document.querySelector('.cheerleader')
+const quoteElement = document.querySelector('.quote')
+const form = document.querySelector('.todo-form')
+const todoInput = document.querySelector('.todo-form input');
+const todoContainer = document.querySelector('.todo-container');
+
 randomNum = Math.floor(Math.random()*500);
 
 luckyNumberElement.textContent = `Your lucky number today is ${randomNum}`
@@ -32,12 +37,15 @@ let cheerleaders = [
     },
 ]
 
+const getRandomIndex = (array) => {
+   let randomIndex =  Math.floor(Math.random()*array.length);
+   return randomIndex
+}
 
-
-let randomIndex = Math.floor(Math.random()*cheerleaders.length)
+let cheerleaderIndex = getRandomIndex(cheerleaders) // gives us 0-3
 
 // select random cheerleader
-let randomCheerleader = cheerleaders[randomIndex];
+let randomCheerleader = cheerleaders[cheerleaderIndex];
 // put on screen
 console.log(randomCheerleader);
 
@@ -46,6 +54,24 @@ cheerleaderParent.children[1].src = randomCheerleader.img;
 
 
 
+const motivationalQuotes = [
+    "Believe in yourself. You are braver than you think, more talented than you know, and capable of more than you imagine.",
+    "Don't watch the clock; do what it does. Keep going.",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    "The only way to do great work is to love what you do.",
+    "You are never too old to set another goal or to dream a new dream.",
+    "The future depends on what you do today.",
+    "Believe you can and you're halfway there.",
+    "Hardships often prepare ordinary people for an extraordinary destiny.",
+    "You are amazing just the way you are.",
+    "The only person you should try to be better than is the person you were yesterday."
+];
+
+let randomQuoteIndex = getRandomIndex(motivationalQuotes); // return 0-10
+
+let randomQuote = motivationalQuotes[randomQuoteIndex]
+// put string on screen
+quoteElement.textContent = randomQuote;
 
 fineButton.addEventListener('click', () => {
     // 1. change img src to ./fine.jpg
@@ -79,4 +105,60 @@ sadButton.addEventListener('click', () => {
     happyButton.classList.remove('active-button')
     // 4. add active class to fine button
     sadButton.classList.add("active-button")
+})
+
+cheerleaderParent.children[1].addEventListener('click', () => {
+    console.log("you clicked the cheerleader!");
+    let newRandomIndex = getRandomIndex(motivationalQuotes);
+    let newRandomQuote = motivationalQuotes[newRandomIndex];
+    quoteElement.textContent = newRandomQuote;
+
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let valueFromInput = todoInput.value;
+    console.log(valueFromInput);
+    // create a new LI element
+    const newTodoElement = `
+    <div class="single-todo">
+        <input type="checkbox" />
+        <p>${valueFromInput}</p>
+        <div class="close">X</div>
+    </div>
+    `
+
+    const divContainer = document.createElement('div')
+    divContainer.classList = "single-todo"
+
+    const  myInput = document.createElement('input')
+    myInput.type = "checkbox"
+
+    divContainer.appendChild(myInput)
+
+    
+    const  pTag = document.createElement('p')
+    pTag.textContent = valueFromInput
+
+    const  closeButton = document.createElement('div')
+    closeButton.classList = "close"
+    closeButton.textContent = "X"
+    closeButton.addEventListener('click', (e) => {
+        console.log("you clicked a button!");
+        console.log(e.target);
+        todoContainer.removeChild(divContainer)
+    })
+
+    divContainer.appendChild(pTag)
+    divContainer.appendChild(closeButton)
+
+
+    // const newTodoElement = document.createElement('li');
+    // put the value as the text content
+    newTodoElement.textContent = valueFromInput;
+    //  PUT INSIDE the container
+    // todoContainer.appendChild(newTodoElement)
+
+    todoContainer.appendChild(divContainer)
+    todoInput.value = "";
 })
